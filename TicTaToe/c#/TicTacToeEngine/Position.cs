cbd0bd9e43a4bf20c68f4842f538a28e8b9540e0
Alexandre Soler Sanandres <alexandre@soler-sanandres.net>
2016-06-10 20:25:04 +0100

    moving all to c# folder

---
+++ TicTaToe/c#/TicTacToeEngine/Position.cs
@@ -0,0 +1,15 @@
+﻿namespace TicTacToeEngine
+{
+    public class Position
+    {
+        public Position(int x, int y)
+        {
+            Y = y;
+            X = x;
+        }
+
+        public int X { get; }
+
+        public int Y { get; }
+    }
+}
---
+++ TicTaToe/c#/TicTacToeEngine/TicTacToeGame.cs
@@ -0,0 +1,50 @@
+﻿namespace TicTacToeEngine
+{
+    public class TicTacToeGame
+    {
+        private readonly string[,] board = new string[3, 3];
+
+        private readonly string NoWinner = "No winner - Game in progress.";
+
+        private string actualValue = string.Empty;
+
+        public TicTacToeGame()
+        {
+            Winner = NoWinner;
+        }
+
+        public string Winner { get; private set; }
+
+        public void PlayAt(Position newPosition)
+        {
+            actualValue = actualValue == "X" ? "O" : "X";
+            board[newPosition.X, newPosition.Y] = actualValue;
+            SetWinner(newPosition);
+        }
+
+        private void SetWinner(Position p)
+        {
+            if (IsVerticalWinnerAtColumn(p.X) || IsHorizontalWinnerAtRow(p.Y) || IsFirstDiagonalWinner() ||
+                IsSecondDiagonalWinner())
+            {
+                Winner = actualValue;
+            }
+        }
+
+        private bool IsSecondDiagonalWinner()
+            => (ValueAt(0, 2) == actualValue && ValueAt(1, 1) == actualValue && ValueAt(2, 0) == actualValue);
+
+        private bool IsFirstDiagonalWinner()
+            => (ValueAt(0, 0) == actualValue && ValueAt(1, 1) == actualValue && ValueAt(2, 2) == actualValue);
+
+        private bool IsHorizontalWinnerAtRow(int y)
+            => ValueAt(0, y) == actualValue && ValueAt(1, y) == actualValue && ValueAt(2, y) == actualValue;
+
+        private bool IsVerticalWinnerAtColumn(int x)
+            => ValueAt(x, 0) == actualValue && ValueAt(x, 1) == actualValue && ValueAt(x, 2) == actualValue;
+
+        public string ValueAt(Position p) => ValueAt(p.X, p.Y);
+
+        private string ValueAt(int x, int y) => board[x, y];
+    }
+}
